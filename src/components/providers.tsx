04c8cd8bd384930c9f5e436a7dc64087fc451773
Chuck Okoroji <dandychux@gmail.com>
2025-05-03 12:40:51 -0400

    Added Plausible tracking

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,18 @@
 'use client';
 
 import { ViewportProvider } from '~/lib/useViewport';
+import PlausibleProvider from 'next-plausible'
 
 type ProviderProps = {
 	children: React.ReactNode;
 };
 
 export const Providers: React.FC<ProviderProps> = ({ children }) => {
-	return <ViewportProvider>{children}</ViewportProvider>;
+	return (
+		<PlausibleProvider domain="bluenomadworld.com" customDomain="https://plausible.blackstacksolutions.com" selfHosted>
+			<ViewportProvider>
+				{children}
+			</ViewportProvider>
+		</PlausibleProvider>
+	);
 };
