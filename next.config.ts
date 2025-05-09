db46790e4112e4433c94a32d8f15e79c64df959d
Chuck Okoroji <dandychux@gmail.com>
2025-05-09 14:01:20 -0400

    updated content security policy

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
 			{
 				hostname: 'cdn.sanity.io',
 				pathname: '/images/**/*'
-			}
+			},
 		]
 	},
 	headers: async () => {
@@ -33,8 +33,9 @@ const nextConfig: NextConfig = {
 					{
 						key: "Content-Security-Policy",
 						// value: "default-src 'self'; img-src 'self' cdn.sanity.io; script-src 'self' cdn.sanity.io; style-src 'self' cdn.sanity.io; font-src 'self' cdn.sanity.io",
-						value: `script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://*.doubleclick.net https://www.googleadservices.com https://www.google.com https://www.gstatic.com https://*.squarecdn.com https://js.squareup.com https://cdn.cookielaw.org https://booking-flow-production-c.squarecdn.com https://square.site https://app.squareup.com 'unsafe-inline'; script-src-attr 'unsafe-inline';`
+						value: `script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://*.doubleclick.net https://www.googleadservices.com https://www.google.com https://www.gstatic.com https://*.squarecdn.com https://js.squareup.com https://cdn.cookielaw.org https://booking-flow-production-c.squarecdn.com https://square.site https://app.squareup.com https://plausible.blackstacksolutions.com 'unsafe-inline'; script-src-attr 'unsafe-inline';`
 					}
+
 				]
 			}
 		]
