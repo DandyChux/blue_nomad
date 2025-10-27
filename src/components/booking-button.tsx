8530c0495274a256756c3f46a6233541a8f95ffd
Chuck Okoroji <dandychux@gmail.com>
2025-10-26 23:40:19 -0400

    style: show close button on booking CTA on mobile

--- src/components/booking-button.tsx
+++ src/components/booking-button.tsx
@@ -27,12 +27,13 @@ export const BookingButton = () => {
 		<div className="fixed bottom-0 right-2 z-50">
 			<div className="relative group">
 				<button
-					className="absolute -top-2 -left-2 h-6 w-6 rounded-full bg-black hover:bg-transparent hover:text-black border-2 border-black text-white items-center justify-center text-xs hidden group-hover:flex hover:cursor-pointer"
+					className="absolute -top-2 -left-2 h-6 w-6 rounded-full bg-black hover:bg-transparent hover:text-black border-2 border-black text-white items-center justify-center text-xs flex md:hidden md:group-hover:flex hover:cursor-pointer"
 					onClick={handleClose}
 					aria-label="Close booking button"
 				>
 					X
 				</button>
+
 				<Button onClick={handleClick} className="rounded-full bg-black text-white border-2 border-black uppercase hover:bg-transparent hover:text-black" size={'xl'} id='booking-button'>
 					<Link
 						href="https://book.squareup.com/appointments/augj56g525h4rw/location/LSP68REJT9SVH/services"
